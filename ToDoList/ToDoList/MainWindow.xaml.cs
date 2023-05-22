1e5be26cb434d734023e4520619b8fae42205aad
Killer31g <109106257+Killer31g@users.noreply.github.com>
2023-05-22 21:20:57 +0200

    Add files via upload

---
+++ ToDoList/ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ToDoListApp
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> tasks;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tasks = new ObservableCollection<string>();
+            taskListBox.ItemsSource = tasks;
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            string newTask = taskTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(newTask))
+            {
+                tasks.Add(newTask);
+                taskTextBox.Clear();
+            }
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                var task = button.DataContext as string;
+                tasks.Remove(task);
+            }
+        }
+
+    }
+}
